94a7a4a6629f52510bf8d7e7641d9b15aafaeb3e
Lê Văn Nhân Đức <121036269+AndyUC@users.noreply.github.com>
2023-12-25 20:25:09 +0700

    Update-1 push.js

--- start/controller/push.js
+++ start/controller/push.js
@@ -10,7 +10,7 @@ const createpush = async (req, res) => {
     const audience = `${parsedUrl.protocol}//${parsedUrl.hostname}`;
     const vapidHeaders = webpush.getVapidHeaders(
       audience,
-      'mailto: example@web-push-node.org',
+      'mailto:nhanduc811@gmail.com',
       "BPnBnQ3-OJ3fwA51Ispz7srlLRefQdKQipkjA4fnzYJnIim4Wc0LHF3-Z-KafgK_XxrO-8eI6rvpbvQ7h12qreo",
       "r4N0hXl_uo62_Dkh9cFgov61Hcw4zumjC54vffKxFXk",
       'aes128gcm'
